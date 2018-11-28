a8a7943c7210a704d12e7d6c4e1078403407da52
Aelexe <Aelexe@gmail.com>
2018-11-28 16:34:10 +1300

    Add .htaccess

--- Gruntfile.js
+++ Gruntfile.js
@@ -115,7 +115,7 @@ module.exports = function(grunt) {
 					{
 						expand: true,
 						cwd: "src",
-						src: "**/*.html",
+						src: ["**/*.html", "**/.htaccess"],
 						dest: "dist"
 					}
 				]
@@ -125,7 +125,7 @@ module.exports = function(grunt) {
 					{
 						expand: true,
 						cwd: "dist",
-						src: "**/*.html",
+						src: ["**/*.html", "**/.htaccess"],
 						dest: DEPLOY_PATH
 					}
 				]
@@ -243,7 +243,7 @@ module.exports = function(grunt) {
 				tasks: ["validateCode", "buildCode", "deployCode"]
 			},
 			html: {
-				files: "src/**/*.html",
+				files: ["src/**/*.html", "src/**/.htaccess"],
 				tasks: ["buildHtml", "deployHtml"]
 			},
 			styles: {
